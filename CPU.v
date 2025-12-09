298cbbeda8b2f4d8303113d4c2582fc8d1e8635e
mzckawa <mzc@cin.ufpe.br>
2025-12-09 16:34:33 -0300

    Feita (necessita revisao) a logica da troca de estados

--- CPU.v
+++ CPU.v
@@ -1,14 +1,16 @@
 // arquivo para a FSM da CPU 
 
-module CPU (input clk, input botao_ligar, input botao_ligar_agora, input botao_enviar_agora, input [2:0] instrucao, output ) 
+module CPU (input clk, input botao_ligar_agora, input botao_enviar_agora, input [2:0] instrucao, output reg flag_clear, ) 
 
-parameter estado_aguardando = 0,
-estado_gravando = 1,
-estado_decodificando = 2,
-estado_executando = 3,
-estado_escrevendo = 4,
-estado_preparando_lcd = 5,
-estado_exibindo = 6;
+parameter estado_desligado = 0,
+estado_ligado = 1,
+estado_aguardando = 2,
+estado_gravando = 3,
+estado_decodificando = 4,
+estado_executando = 5,
+estado_escrevendo = 6,
+estado_preparando_lcd = 7,
+estado_exibindo = 8;
 
 reg [2:0] estado; 
 
@@ -28,28 +30,70 @@ always@(posedge clk) begin
     if(flag_ligar) begin
 
         sistema_ligado <= ~sistema_ligado; // em qualquer situação, o botão vai inverter o atual estado do sistema
+        // lembrete: essa inversão ocorrerá, somente, ao FINAL do ciclo do clock 
 
-        // se, após a inversão, sistema_ligado==1, significa que precisamos LIGAR a CPU
+        // se o sistema estava ligado, vamos desligá-lo (a lógica abaixo irá acontecer antes do bit sistema_ligado ser invertido)
+        if(sistema_ligado) begin
+            estado <= estado_desligado;
+        end
+
+        else begin
+            estado <= estado_ligado;
+        end
+
+        // agora, sim, o corpo majoritário da lógica da FSM da CPU
+
         if(sistema_ligado) begin
             
+            case(estado)
 
-    end
+            estado_desligado: begin
+                // vazio, indicando que não deve ser feita nenhuma mudança
+            end
 
+            estado_ligado: begin
+                estado <= estado_aguardando;
+            end 
 
-    
-    case(estado) 
+            estado_aguardando begin
+                estado <= (flag_enviar) ? estado_decodificando : estado_aguardando;
+            end 
 
-    if(estado == estado_desligado) begin
-        if(detector_ligar) begin
+            estado_gravando: begin
+                estado <= estado_decodificando;
+            end 
+
+            estado_decodificando: begin
+                estado <= estado_executando;
+            end
+
+            estado_executando: begin 
+                estado <= estado_escrevendo;
+            end
+
+            estado_escrevendo: begin
+                estado <= estado_preparando_lcd;
+            end
+
+            estado_preparando_lcd: begin
+                estado <= estado_exibindo;
+            end
+
+            estado_exibindo: begin
+                estado <= estado_aguardando;
+            end
         
-        end
-    end 
-    endcase 
-
-
+            endcase
+        end 
+    end
 end
 
 // Lógica das saídas 
+always@(posedge clk) begin
 
+if(estado == estado_ligado) begin 
+    // lógica de ace
+end
+end
 
 endmodule
